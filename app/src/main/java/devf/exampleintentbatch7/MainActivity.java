ea04074ee5c6c76ce8f219f3bba8025902c5efb7
Salvador <salvadoredgar99@gmail.com>
2015-11-03 18:57:44 -0600

    Add arguments intents

--- app/src/main/java/devf/exampleintentbatch7/MainActivity.java
+++ app/src/main/java/devf/exampleintentbatch7/MainActivity.java
@@ -6,16 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextSome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button_start_activity);
-        button.setOnClickListener(this);
+        Button buttonApplication = (Button) findViewById(R.id.button_start_activity);
+        buttonApplication.setOnClickListener(this);
+
+        Button buttonActivity = (Button) findViewById(R.id.button_start_activity);
+        buttonActivity.setOnClickListener(this);
+
+        editTextSome = (EditText) findViewById(R.id.edittext_some);
 
     }
 
@@ -32,9 +40,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.button_start_activity:
-                Intent intentActivity = new Intent(this, SecondActivity.class);
-                startActivity(intentActivity);
 
+                Intent intentActivity = new Intent(this, SecondActivity.class);
+                String textSome = editTextSome.getText().toString();
+                intentActivity.putExtra("my_key", textSome);
+
+                startActivity(intentActivity);
                 break;
         }
     }
--- app/src/main/java/devf/exampleintentbatch7/SecondActivity.java
+++ app/src/main/java/devf/exampleintentbatch7/SecondActivity.java
@@ -1,7 +1,9 @@
 package devf.exampleintentbatch7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String textSome = intent.getStringExtra("my_key");
+
+        TextView textViewSecondActivity = (TextView) findViewById(R.id.text_view_second_activity);
+        textViewSecondActivity.setText(textSome);
+
     }
 }
